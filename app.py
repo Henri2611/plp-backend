53bbb839194a2839aa6c2dfc98243ade56efc20b
henri ouma <147258392+Henri2611@users.noreply.github.com>
2025-09-01 23:00:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -118,4 +118,4 @@ def generate_recipes():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)) debug=True )
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True )
